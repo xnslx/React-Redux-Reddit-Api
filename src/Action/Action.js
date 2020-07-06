a8fb68fa961ebb527c12176a22d34b2159e62a99
Xian Li <xnslx89@gmail.com>
2020-07-05 23:17:50 -0500

    action.js

--- src/Action/Action.js
+++ src/Action/Action.js
@@ -1,47 +1,65 @@
 import axios from 'axios';
 
-export const GET_POSTS = 'GET_POST';
-export const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
-export const GET_POSTS_FAIL = 'GET_POSTS_FAIL';
-export const SELECT_OPTION = 'SELECT_POSTS';
+export const REQUEST_POSTS = "REQUEST_POST";
+export const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS";
+export const GET_POSTS_FAIL = "GET_POSTS_FAIL";
+export const USER_INPUT = "USER_INPUT";
+export const SELECT_SORT = "SELECT_SORT";
+export const SELECT_LIMIT = "SELECT_LIMIT";
 
-export const selectOption = (value) => {
-    return {
-        type:SELECT_OPTION,
-        value
-    }
-}
-export const getPosts = (posts) => {
-    return {
-        type: GET_POSTS,
-        posts
-    }
-}
+export const userInput = input => {
+  return {
+    type: USER_INPUT,
+    input
+  };
+};
 
-export const getPostsSuccess = (json) => {
-    return {
-        type: GET_POSTS_SUCCESS,
-        posts: json.data.children.map(child=>child.data),
-    }
-}
+export const selectSORT = sortOption => {
+  return {
+    type: SELECT_SORT,
+    sortOption
+  };
+};
+
+export const selectLimit = limitOption => {
+  return {
+    type: SELECT_LIMIT,
+    limitOption
+  };
+};
+export const requestPosts = posts => {
+  return {
+    type: REQUEST_POSTS,
+    posts
+  };
+};
+
+export const getPostsSuccess = json => {
+  return {
+    type: GET_POSTS_SUCCESS,
+    posts: json.data.children.map(child => child.data)
+  };
+};
 
 export const getPostsFail = () => {
-    return {
-        type: GET_POSTS_FAIL
-    }
-}
+  return {
+    type: GET_POSTS_FAIL
+  };
+};
 
 export const fetchPosts = (query, sortBy, limit, posts) => {
-    return dispatch => {
-        dispatch(getPosts(posts))
-        return axios.get(`https://www.reddit.com/r/subreddit/search.json?q={query}&sort={sortBy}&limit={limit} `)
-            .then((res) => {
-                res.json()
-            })
-            .then((json) => dispatch(getPostsSuccess(posts, json)))
-            .catch((err) => {
-                dispatch(getPostsFail(err))
-            })
-    }
-}
-
+  return dispatch => {
+    dispatch(requestPosts(posts));
+    return axios
+      .get(
+        `https://www.reddit.com/r/subreddit/search.json?q={query}&sort={sortBy}&limit={limit} `
+      )
+      .then(res => {
+        res.json();
+      })
+      .then(json => dispatch(getPostsSuccess(posts, json)))
+      .catch(err => {
+        dispatch(getPostsFail(err));
+      });
+  };
+};
